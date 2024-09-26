6f25a053a1199a87425f57c66a2862965edbc535
Yassine_Ibhir <yibhir101@gmail.com>
2024-09-26 11:11:23 -0400

    added project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# delivery-system-app
+##### To start the project along with me just download the start-up file and run available script
+
+```javascript
+// first install all necessary dependencies
+
+npm i
+
+// next run
+
+npm start
+
+```
---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Helmet from "../components/Helmet/Helmet.js";
+import { Container, Row, Col } from "reactstrap";
+
+import { Link } from "react-router-dom";
+
+import guyImg from "../assets/images/delivery-guy.png";
+import "../styles/hero-section.css";
+
+const Home = () => {
+  return (
+    <Helmet title="Home">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <h5 className="mb-3">Fast & Reliable</h5>
+                <h1 className="mb-4 hero__title">
+                  <span>Trust</span> us to deliver your package
+                </h1>
+
+                <button className="order__btn d-flex align-items-center justify-content-between ">
+                  <Link to="/pizzas">
+                    Request a Quote <i className="ri-arrow-right-s-line"></i>
+                  </Link>
+                </button>
+              </div>
+            </Col>
+
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={guyImg} alt="delivery-guy" className="w-100" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Home;
